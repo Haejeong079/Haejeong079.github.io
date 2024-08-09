b39aebd65851e21d2b333c28b0e110b33b493d7e
Haejeong079 <127839004+Haejeong079@users.noreply.github.com>
2024-08-09 10:21:55 +0900

    change config

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1723163304',resources: [
+    cacheName: 'chirpy-1723165554',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
